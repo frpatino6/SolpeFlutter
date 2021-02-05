46cde8c1a7104879a490b94f56f900988bf9b25a
frodriguez <frpatino6@gmail.com>
2021-02-05 05:55:50 -0500

    RefactorDirectory

--- android/app/src/main/kotlin/com/solpe/SolpeFlutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.solpe.SolpeFlutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/solpe/rcn_solpe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.solpe.rcn_solpe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
